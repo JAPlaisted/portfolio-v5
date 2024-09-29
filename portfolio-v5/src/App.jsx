8ab6c53b266add3aba65f0846eecac2b34932202
JAPlaisted <jonathan.plaisted@outlook.com>
2024-09-29 16:01:52 -0700

    start new portfolio

---
+++ portfolio-v5/src/App.jsx
@@ -0,0 +1,13 @@
+import { FaReact } from 'react-icons/fa';
+import  './App.scss';
+
+function App() {
+  return (
+    <div className="appContainer">
+      <h1>Welcome to My Portfolio</h1>
+      <FaReact size={50} color="#61DBFB" />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio-v5/src/colors.js
@@ -0,0 +1,8 @@
+const colors = {
+    primary: '#FF6138',
+    secondary: '#FFBE53',
+    accent: '#2980B9',
+    dark: '#282741',
+};
+  
+export default colors;
